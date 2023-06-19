f60f56be220a263dc32cf6d6dff0870b4ff8b461
Emil Ivanov <emil@receipt-bank.com>
2023-06-19 12:59:06 +0300

    Bump crystal-db from 0.11.0 to master

--- src/db.cr
+++ src/db.cr
@@ -3,12 +3,12 @@ require "db"
 CONNECTION = "postgres://postgres@localhost/crystal_load_test?initial_pool_size=5&max_pool_size=5&max_idle_pool_size=5"
 
 class Film
-    include JSON::Serializable
-  
-    DB.mapping({
-      id: Int32,
-      title: String,
-      description: String,
-      release_year: Int32
-    })
-  end
+  include JSON::Serializable
+
+  DB.mapping({
+    id: Int32,
+    title: String,
+    description: String,
+    release_year: Int32
+  })
+end
